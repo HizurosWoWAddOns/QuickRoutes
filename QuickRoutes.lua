fab52ec36b7ddadb283a36392b51947db462321a
Hizuro <hizuro@gmx.net>
2016-08-31 02:53:27 +0200

    added optional hints in tooltip, added optional second tooltip, updated toc file

--- QuickRoutes.lua
+++ QuickRoutes.lua
@@ -5,12 +5,11 @@ local QuickRoutes = CreateFrame("frame")
 
 local LibQTip = LibStub('LibQTip-1.0')
 
-local tooltip
+local tooltip, tooltip2
 local LDB_ANCHOR
 local GROUP_CHECKMARK	= "|TInterface\\Buttons\\UI-CheckBox-Check:0|t"
 
-local LDB = LibStub("LibDataBroker-1.1"):NewDataObject("QuickRoutes",
-{
+local LDB = LibStub("LibDataBroker-1.1"):NewDataObject("QuickRoutes",{
 	type	=	"launcher",
 	label	=	"Routes",
 	text	=	"Routes",
@@ -19,6 +18,7 @@ local LDB = LibStub("LibDataBroker-1.1"):NewDataObject("QuickRoutes",
 		if button == "LeftButton" then
 			LibStub("AceConfigDialog-3.0"):Open("Routes")
 		else
+			InterfaceOptionsFrame_OpenToCategory("QuickRoutes")
 			InterfaceOptionsFrame_OpenToCategory("QuickRoutes")
 		end
 	end,
@@ -52,6 +52,20 @@ local options = {
 				if v then LDBIcon:Hide("QuickRoutes") else LDBIcon:Show("QuickRoutes") end
 			end,
 		},
+		show_hints = {
+			type = "toggle", width = "double",
+			name = L["Show hints"],
+			desc = L["Display hints in tooltip"],
+			get = function() return QuickRoutesDB.hints; end,
+			set = function(_,v) QuickRoutesDB.hints = v; end
+		},
+		show_routetooltip = {
+			type = "toggle", width = "double",
+			name = L["Show second tooltip"],
+			desc = L["Display second tooltip with additional informations to a single route"],
+			get = function() return QuickRoutesDB.tooltip2; end,
+			set = function(_,v) QuickRoutesDB.tooltip2 = v; end
+		}
 	},
 }
 
@@ -61,7 +75,49 @@ LibStub("AceConfigDialog-3.0"):AddToBlizOptions("QuickRoutes")
 local zoneTable = {}
 local zoneData
 local zone
+local function C(color,str)
+	return "|cff"..( ({ltblue="69ccf0",ltgreen="80ff80",ltyellow="fff569",dkyellow="ffcc00",copper="f0a55f"})[color] or "ffffff" )..str.."|r";
+end
 
+local function useTipTac(tt)
+	-- Tiptac Support for LibQTip Tooltips
+	if _G.TipTac and _G.TipTac.AddModifiedTip then
+		-- Pass true as second parameter because hooking OnHide causes C stack overflows
+		_G.TipTac:AddModifiedTip(tt, true);
+	end
+end
+
+local function Entry_OnEnter(self, data, button)
+	if LibQTip:IsAcquired("QuickRoutes2") then
+		tooltip2:Clear()
+	else
+		tooltip2 = LibQTip:Acquire("QuickRoutes2", 2, "LEFT", "RIGHT");
+		tooltip2:SmartAnchorTo(tooltip)
+		useTipTac(tooltip2)
+	end
+
+	tooltip2:AddHeader(C("dkyellow",L["Route"]..":"),data.name);
+	tooltip2:AddSeparator();
+
+	local cat, obj = C("ltblue",CATEGORIES..":"), C("ltblue",L["Objects"]..":");
+	for k,v in pairs(Routes.db.global.routes[zone][data.name].db_type)do
+		tooltip2:AddLine(cat,C("ltgreen",k));
+		cat = " ";
+	end
+
+	tooltip2:AddSeparator(1,1,1,1,.5);
+
+	for k,v in pairs(Routes.db.global.routes[zone][data.name].selection)do
+		tooltip2:AddLine(obj,C("ltgreen",v));
+		obj = " ";
+	end
+
+	tooltip2:Show();
+end
+
+local function Entry_OnLeave(self)
+	tooltip2:Hide();
+end
 
 local function Entry_OnMouseUp(frame, route_name, button)
 	Routes.db.global.routes[zone][route_name].hidden = not Routes.db.global.routes[zone][route_name].hidden
@@ -70,21 +126,6 @@ local function Entry_OnMouseUp(frame, route_name, button)
 	LDB.OnEnter(LDB_ANCHOR)
 end
 
--- Setup the Title Font. 14
-local ssTitleFont = CreateFont("ssTitleFont")
-ssTitleFont:SetTextColor(1,0.823529,0)
-ssTitleFont:SetFont(GameTooltipText:GetFont(), 14)
-
--- Setup the Header Font. 12
-local ssHeaderFont = CreateFont("ssHeaderFont")
-ssHeaderFont:SetTextColor(1,0.823529,0)
-ssHeaderFont:SetFont(GameTooltipHeaderText:GetFont(), 12)
-
--- Setup the Regular Font. 12
-local ssRegFont = CreateFont("ssRegFont")
-ssRegFont:SetTextColor(1,0.823529,0)
-ssRegFont:SetFont(GameTooltipText:GetFont(), 12)
-
 function LDB.OnEnter(self)
 	LDB_ANCHOR = self
 
@@ -92,20 +133,14 @@ function LDB.OnEnter(self)
 		tooltip:Clear()
 	else
 		tooltip = LibQTip:Acquire("QuickRoutes", 2, "RIGHT", "LEFT")
-
-		tooltip:SetBackdropColor(0,0,0,1)
-
-		tooltip:SetHeaderFont(ssHeaderFont)
-		tooltip:SetFont(ssRegFont)
-
 		tooltip:SmartAnchorTo(self)
 		tooltip:SetAutoHideDelay(0.25, self)
+		useTipTac(tooltip)
 	end
 
-	local line = tooltip:AddLine()
-	tooltip:SetCell(line, 1, "QuickRoutes", ssTitleFont , "CENTER", 0)
+	tooltip:SetCell(tooltip:AddLine(), 1, C("dkyellow","QuickRoutes"), tooltip:GetHeaderFont() , "CENTER", 0)
 	tooltip:AddSeparator()
-	tooltip:AddLine(" ")
+	tooltip:AddSeparator(4,0,0,0,0);
 
 	zone = GetRealZoneText()
 	if Routes.LZName[zone] then
@@ -130,15 +165,21 @@ function LDB.OnEnter(self)
 			checked = not route_data.hidden
 
 			if route_data.route and #route_data.route > 0 then
-				line = tooltip:AddLine()
-				if checked then
-					tooltip:SetCell(line, 1, GROUP_CHECKMARK)
-				end
-				tooltip:SetCell(line, 2, "|cff82c5ff" .. route_name)
+				line = tooltip:AddLine(checked and GROUP_CHECKMARK or " ",  C("ltblue",route_name));
 				tooltip:SetLineScript(line, "OnMouseUp", Entry_OnMouseUp, route_name)
+				if QuickRoutesDB.tooltip2 then
+					tooltip:SetLineScript(line, "OnEnter", Entry_OnEnter, {tt=tooltip, name=route_name})
+					tooltip:SetLineScript(line, "OnLeave", Entry_OnLeave)
+				end
 			end
 		end
 
+	end
+
+	if QuickRoutesDB.hints then
+		tooltip:AddSeparator(4,0,0,0,0);
+		tooltip:SetCell(tooltip:AddLine(),1,C("copper",L["Left-click"]).." || "..C("ltgreen",L["Open routes"]),nil,"CENTER",0)
+		tooltip:SetCell(tooltip:AddLine(),1,C("copper",L["Right-click"]).." || "..C("ltgreen",L["Open options"]),nil,"CENTER",0)
 	end
 
 	tooltip:UpdateScrolling()
@@ -149,7 +190,6 @@ function QuickRoutes:PLAYER_LOGIN()
 	if not QuickRoutesDB then
 		QuickRoutesDB = {}
 	end
-
 	if not QuickRoutesDB.MinimapIcon then
 		QuickRoutesDB.MinimapIcon = {
 			hide = false,
@@ -157,7 +197,12 @@ function QuickRoutes:PLAYER_LOGIN()
 			radius = 80,
 		}
 	end
-
+	if QuickRoutesDB.hints==nil then
+		QuickRoutesDB.hints = true;
+	end
+	if QuickRoutesDB.tooltip2==nil then
+		QuickRoutesDB.tooltip2 = true;
+	end
 	if LDBIcon then
 		LDBIcon:Register("QuickRoutes", LDB, QuickRoutesDB.MinimapIcon)
 	end
--- localization.lua
+++ localization.lua
@@ -5,25 +5,99 @@ ns.L=L;
 
 --[[
 
-L["Quickly set and unset routes."] = "";
+L["Display hints in tooltip"] = "";
+L["Display second tooltip with additional informations to a single route"] = "";
 L["Hide Minimap Icon"] = "";
+L["Left-click"] = "";
+L["Objects"] = "";
+L["Open options"] = "";
+L["Open routes"] = "";
+L["Quickly set and unset routes."] = "";
+L["Right-click"] = "";
+L["Route"] = "";
+L["Show hints"] = "";
 L["Show or hide the minimap icon."] = "";
+L["Show second tooltip"] = "";
 
 ]]
 
+if LOCALE_enUS or LOCALE_enGB then
+	L["GathererMINE"] = "Mining"
+	L["GathererHERB"] = "Herbalism"
+	L["GathererOPEN"] = "Treasure"
+	L["GatherMate2Herb Gathering"] = "Herbalism"
+	L["GatherMate2Mining"] = "Mining"
+	L["GatherMate2Fishing"] = "Fishing"
+	L["GatherMate2Extract Gas"] = "Extract Gas"
+	L["GatherMate2Treasure"] = "Treasure"
+	L["GatherMate2Archaeology"] = "Archaeology"
+	L["GatherMate2Logging"] = "Logging"
+end
+
 if LOCALE_deDE then
 	-- For german translations...
-	--L["Quickly set and unset routes."] = "";
+	L["Display hints in tooltip"] = "Zeigt Tipps im Tooltip";
+	L["Display second tooltip with additional informations to a single route"] = "Zeigt zweiten Tooltip mit zusätzlichen Informationen zu einer einzelnen Route";
 	L["Hide Minimap Icon"] = "Verstecke Minimap Symbol";
+	L["Left-click"] = "Linksklick";
+	L["Objects"] = "Objekte";
+	L["Open options"] = "Öffnet Optionen";
+	L["Open routes"] = "Öffnet Routes";
+	--L["Quickly set and unset routes."] = "";
+	L["Right-click"] = "Rechtsklick";
+	L["Route"] = "Route";
+	L["Show hints"] = "Zeige Tipps";
 	L["Show or hide the minimap icon."] = "Zeige oder verstecke das Minimap Symbol.";
+	L["Show second tooltip"] = "Zeige zweiten Tooltip";
+	--
+	--
+	L["Fishing"] = "Angeln"
+	L["GathererHERB"] = "Kräuterkunde"
+	L["GathererMINE"] = "Bergbau"
+	L["GathererOPEN"] = "Schätze"
+	L["GatherMate2Archaeology"] = "Archäologie"
+	L["GatherMate2Extract Gas"] = "Gas"
+	L["GatherMate2Fishing"] = "Angeln"
+	L["GatherMate2Herb Gathering"] = "Kräuterkunde"
+	L["GatherMate2Logging"] = "Holzfällen"
+	L["GatherMate2Mining"] = "Bergbau"
+	L["GatherMate2Treasure"] = "Schätze"
+	L["Herbalism"] = "Kräuterkunde"
+	L["Mining"] = "Bergbau"
 end
 
 if LOCALE_esES or LOCALE_esMX then
 	-- For spanish translations...
+	L["Fishing"] = "Pesca"
+	L["GathererHERB"] = "Herbología"
+	L["GathererMINE"] = "Minería"
+	L["GathererOPEN"] = "Tesoro"
+	L["GatherMate2Archaeology"] = "Arqueología"
+	L["GatherMate2Extract Gas"] = "Extraer Gas"
+	L["GatherMate2Fishing"] = "Pesca"
+	L["GatherMate2Herb Gathering"] = "Herbología"
+	--L["GatherMate2Logging"] = ""
+	L["GatherMate2Mining"] = "Minería"
+	L["GatherMate2Treasure"] = "Tesoro"
+	L["Herbalism"] = "Herbología"
+	L["Mining"] = "Minería"
 end
 
 if LOCALE_frFR then
 	-- For french translations...
+	L["Fishing"] = "Pêche"
+	L["GathererHERB"] = "Herboristerie"
+	L["GathererMINE"] = "Minage"
+	L["GathererOPEN"] = "Trésor"
+	L["GatherMate2Archaeology"] = "Archéologie"
+	L["GatherMate2Extract Gas"] = "Gaz"
+	L["GatherMate2Fishing"] = "Pêche"
+	L["GatherMate2Herb Gathering"] = "Herboristerie"
+	--L["GatherMate2Logging"] = ""
+	L["GatherMate2Mining"] = "Minage"
+	L["GatherMate2Treasure"] = "Trésor"
+	L["Herbalism"] = "Herboristerie"
+	L["Mining"] = "Minage"
 end
 
 if LOCALE_itIT then
@@ -39,13 +113,52 @@ end
 
 if LOCALE_ruRU then
 	-- For russian translations...
+	L["Fishing"] = "Рыбалка"
+	L["GathererHERB"] = "Травничество"
+	L["GathererMINE"] = "Горное дело"
+	L["GathererOPEN"] = "Сокровища"
+	L["GatherMate2Archaeology"] = "Археология"
+	L["GatherMate2Extract Gas"] = "Извлечение газа"
+	L["GatherMate2Fishing"] = "Рыболовство"
+	L["GatherMate2Herb Gathering"] = "Травничество"
+	-- L["GatherMate2Logging"] = ""
+	L["GatherMate2Mining"] = "Горное дело"
+	L["GatherMate2Treasure"] = "Сокровища"
+	L["Herbalism"] = "Травничество"
+	L["Mining"] = "Горное дело"
 end
 
 if LOCALE_zhCN then
 	-- For simplified chinese translations...
+	L["Fishing"] = "钓鱼"
+	L["GathererHERB"] = "草药"
+	L["GathererMINE"] = "采矿"
+	L["GathererOPEN"] = "宝藏"
+	L["GatherMate2Archaeology"] = "考古"
+	L["GatherMate2Extract Gas"] = "气体云"
+	L["GatherMate2Fishing"] = "钓鱼"
+	L["GatherMate2Herb Gathering"] = "草药"
+	L["GatherMate2Logging"] = "日志记录"
+	L["GatherMate2Mining"] = "采矿"
+	L["GatherMate2Treasure"] = "宝藏"
+	L["Herbalism"] = "草药"
+	L["Mining"] = "采矿"
 end
 
 if LOCALE_zhTW then
 	-- For traditional chinese translations...
+	L["Fishing"] = "釣魚"
+	L["GathererHERB"] = "採藥"
+	L["GathererMINE"] = "採礦"
+	L["GathererOPEN"] = "寶藏"
+	L["GatherMate2Archaeology"] = "考古"
+	L["GatherMate2Extract Gas"] = "氣雲"
+	L["GatherMate2Fishing"] = "釣魚"
+	L["GatherMate2Herb Gathering"] = "採藥"
+	-- L["GatherMate2Logging"] = ""
+	L["GatherMate2Mining"] = "採礦"
+	L["GatherMate2Treasure"] = "寶藏"
+	L["Herbalism"] = "採藥"
+	L["Mining"] = "採礦"
 end
 
