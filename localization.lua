da8e8d471dcce160e6e62fafe1b066e673768015
Hizuro <hizuro@gmx.net>
2022-06-13 04:33:49 +0200

    update localization file

--- localization.lua
+++ localization.lua
@@ -3,8 +3,8 @@ local addon, ns = ...;
 local L=setmetatable({},{__index=function(t,k) local v="?"; if(k) then v=tostring(k); rawset(t,k,v); end return v; end});
 ns.L=L;
 
--- Hi. This addon needs your help for localization. :)
--- https://wow.curseforge.com/projects/quickroutes/localization
+-- Do you want to help localize this addon?
+-- https://www.curseforge.com/wow/addons/@cf-project-name@/localization
 
 L["Archaeology"] = PROFESSIONS_ARCHAEOLOGY;
 L["Fishing"] = PROFESSIONS_FISHING;
@@ -24,91 +24,26 @@ L["GatherMate2Treasure"] = L["Treasure"];
 L["GatherMate2Archaeology"] = L["Archaeology"];
 L["GatherMate2Logging"] = L["Logging"];
 
---@do-not-package@
-L["AddOnLoaded"] = "AddOn loaded..."
-L["Authors"] = "AddOn authors:"
-L["Description"] = "Gives you quick access to show/hide created routes in current zone"
-L["Hints"] = "Show hints"
-L["HintsDesc"] = "Display hints in tooltip"
-L["Info"] = "Info"
-L["LeftClick"] = "Left-click"
-L["Minimap"] = "Minimap"
-L["NoRouteData"] = "Oops... There are no data in this route"
-L["NoRoutesInZone"] = "No routes found for current zone"
-L["Objects"] = ""
-L["OnLogin"] = "On login"
-L["OpenOptions"] = "Open options"
-L["OpenRoutes"] = "Open routes"
-L["RightClick"] = "Right-click"
-L["Route"] = ""
-L["SecondTT"] = "Show second tooltip"
-L["SecondTTDesc"] = "Display second tooltip with additional informations to a single route"
-L["ShowAddOnLoaded"] = "Show 'AddOn loaded'"
-L["ShowAddOnLoadedDesc"] = "Show 'AddOn loaded...' message on login"
-L["ShowMinimap"] = "Show minimap icon"
-L["ShowMinimapDesc"] = "Show or hide the minimap icon"
-L["Tooltip"] = "Tooltip"
-L["Version"] = "AddOn version:"
-L["QueueFirstNode"] = "Start"
-L["RemoveQueuedNode"] = "Stop"
-L["SetClosestWaypoint"] = "Change|ndirection"
-L["WaypointsTomTom"] = "Waypoints (TomTom)"
-L["WaypointsTomTomDesc"] = "Display quick access to routes \"Waypoints (TomTom)\" options in tooltip."
---@end-do-not-package@
-
 --@localization(locale="enUS", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 
 if LOCALE_deDE then
-	--@do-not-package@
-	L["AddOnLoaded"] = "AddOn geladen..."
-	L["Authors"] = "AddOn Autoren:"
-	L["SetClosestWaypoint"] = "Richtung|nwechseln"
-	L["Description"] = "Gibt dir Schnellzugriff zum anzeigen/verstecken von erstellten Routen der aktuellen Zone"
-	L["Hints"] = "Zeige Tipps"
-	L["HintsDesc"] = "Zeigt Tipps im Tooltip"
-	L["Info"] = "Info"
-	L["LeftClick"] = "Linksklick"
-	L["Minimap"] = "Minikarte"
-	L["NoRouteData"] = "Oops... Da sind keine Daten in der Route"
-	L["NoRoutesInZone"] = "Keine Routen gefunden für aktuelle Zone"
-	L["Objects"] = "Objekte"
-	L["OnLogin"] = "Beim Login"
-	L["OpenOptions"] = "Optionen öffnen"
-	L["OpenRoutes"] = "Routes öffnen"
-	L["QueueFirstNode"] = "Start"
-	L["RemoveQueuedNode"] = "Stopp"
-	L["RightClick"] = "Rechtsklick"
-	L["Route"] = "Route"
-	L["SecondTT"] = "Zeige zweiten Tooltip"
-	L["SecondTTDesc"] = "Zeigt zweiten Tooltip mit zusätzlichen Informationen zu einer einzelnen Route"
-	L["ShowAddOnLoaded"] = "Zeige 'AddOn geladen'"
-	L["ShowAddOnLoadedDesc"] = "Zeige 'AddOn geladen...' Nachricht beim Login"
-	L["ShowMinimap"] = "Zeige Minimap Symbol"
-	L["ShowMinimapDesc"] = "Zeige oder verstecke das Minimap Symbol."
-	L["Tooltip"] = "Tooltip"
-	L["Version"] = "AddOn Version:"
-	L["WaypointsTomTom"] = "Wegpunkte (TomTom)"
-	L["WaypointsTomTomDesc"] = "Zeigt Schnellzugriff auf Routes \"Wegpunkte (TomTom)\" Optionen im Tooltip."
-	--@end-do-not-package@
-	--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="deDE", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_esES then
-	--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_esMX then
-	--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="esMX", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_frFR then
-	--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_itIT then
-	--@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="itIT", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_koKR then
-	--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
-elseif LOCALE_ptBR then
-	--@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+elseif LOCALE_ptBR or LOCALE_ptPT then
+--@localization(locale="ptBR", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_ruRU then
-	--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="ruRU", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_zhCN then
-	--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="zhCN", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 elseif LOCALE_zhTW then
-	--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
+--@localization(locale="zhTW", format="lua_additive_table", handle-subnamespaces="none", handle-unlocalized="ignore")@
 end
-
-
